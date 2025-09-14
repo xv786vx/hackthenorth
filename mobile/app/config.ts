c2b416f2d88208ee08addcd9506bb081f5bd4730
fayaz-rafin <fayaz.rafin@gmail.com>
2025-09-14 06:52:42 +0100

    patched config.ts

--- mobile/app/config.ts
+++ mobile/app/config.ts
@@ -1,6 +1,4 @@
 // Set this to your Pi's LAN IP + port
 export const BACKEND_URL = "http://10.37.103.50:8000";
 export const WS_URL = "ws://10.37.103.50:8000/ws/camera";
-// Set this to your Pi's LAN IP + port
-export const BACKEND_URL = "http://10.37.103.50:8000";
-export const WS_URL = "ws://10.37.103.50:8000/ws/camera";
+
